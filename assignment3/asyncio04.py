901230ee8c639b261a53ba0f3e339a3869df06c2
Chanarongpimket <87462232+Chanarongpimket@users.noreply.github.com>
2024-06-28 11:11:56 +0700

    update asyncio04

--- assignment3/asyncio04.py
+++ assignment3/asyncio04.py
@@ -4,7 +4,6 @@ from time import ctime
 
 async def wash(basket):
     print(f'{ctime()} : Washing Machine ({basket}): Start washing...')
-    await asyncio.sleep(5)
     print(f'{ctime()} : Washing Machine ({basket}): Put the coin')
     await asyncio.sleep(5)
     print(f'{ctime()} : Washing Machine ({basket}): Finished washing')
@@ -13,10 +12,15 @@ async def wash(basket):
 async def main():
     coro = wash('Basket A')
     print(f"{ctime()} : {coro}")
+    print(f"{ctime()} : {type(coro)}")
+    # create task
     task = asyncio.create_task(coro)
     print(f"{ctime()} : {task}")
+    print(f"{ctime()} : {type(task)}")
+    # run task
     result = await task
     print(f"{ctime()} : {result}")
+    print(f"{ctime()} : {type(result)}")
 
 if __name__ == '__main__':
     asyncio.run(main())
