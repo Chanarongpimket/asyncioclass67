706072504e9d87253d1df2e592d0ffcd92f97b84
Chanarongpimket <87462232+Chanarongpimket@users.noreply.github.com>
2024-08-30 10:47:40 +0700

    Done Assign 10

--- assignment10/asyncio02.py
+++ assignment10/asyncio02.py
@@ -1,9 +1,11 @@
-# example of using an asyncio queue without blocking
 from random import random
 import asyncio
- 
+import time
+
 # coroutine to generate work
 async def producer(queue):
+    start_times = time.time()
+
     print('Producer: Running')
     # generate work
     for i in range(10):
@@ -16,10 +18,14 @@ async def producer(queue):
         # add to the queue
         print(f"> Producer put {value}")
         await queue.put(value)
-    # send an all done signal
+    # send an all-done signal
     await queue.put(None)
     print('Producer: Done')
- 
+
+    end_times = time.time()
+    total_time = end_times - start_times
+    return total_time
+
 # coroutine to consume work
 async def consumer(queue):
     print('Consumer: Running')
@@ -39,13 +45,20 @@ async def consumer(queue):
         print(f'\t> Consumer got {item}')
     # all done
     print('Consumer: Done')
- 
+
 # entry point coroutine
 async def main():
+    # start time for the entire program
+    program_start_time = time.time()
     # create the shared queue
-    queue = asyncio.Queue()
+    queue = asyncio.Queue(maxsize=0)
     # run the producer and consumers
-    await asyncio.gather(producer(queue), consumer(queue))
- 
+    producer_time = await asyncio.gather(producer(queue), consumer(queue))
+    # end time for the entire program
+    program_end_time = time.time()
+
+    print(f'Total time taken by producer: {producer_time[0]:.4f} seconds')
+    print(f'Total time of the entire program: {program_end_time - program_start_time:.4f} seconds')
+
 # start the asyncio program
 asyncio.run(main())
