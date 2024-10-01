182e20d29cd52108ec8592ca5056463c7007731f
Chanarongpimket <87462232+Chanarongpimket@users.noreply.github.com>
2024-10-01 19:42:42 +0700

    assignment11

--- assignment11/taskgroup03.py
+++ assignment11/taskgroup03.py
@@ -3,60 +3,65 @@ import asyncio
 from asyncio import Queue
 from random import randrange
 
-# we first implement the Customer and Product classes, 
-# representing customers and products that need to be checked out. 
-# The Product class has a checkout_time attribute, 
-# which represents the time required for checking out the product.
 class Product:
     def __init__(self, product_name: str, checkout_time: float):
         self.product_name = product_name
         self.checkout_time = checkout_time
+
 
 class Customer:
     def __init__(self, customer_id: int, products: list[Product]):
         self.customer_id = customer_id
         self.products = products
 
-# we implement a checkout_customer method that acts as a consumer.
-# As long as there is data in the queue, this method will continue to loop. 
-# During each iteration, it uses a get method to retrieve a Customer instance. 
-# 
-# If there is no data in the queue, it will wait. 
-# 
-# After retrieving a piece of data (in this case, a Customer instance), 
-# it iterates through the products attribute and uses asyncio.sleep to simulate the checkout process.
-# 
-# After finishing processing the data, 
-# we use queue.task_done() to tell the queue that the data has been successfully processed.
 async def checkout_customer(queue: Queue, cashier_number: int):
+    final_time = []
+    final_q = []
+    while not queue.empty():
+        customer: Customer = await queue.get()
+        customer_start_time = time.perf_counter()
+        print(f"the Cashier {cashier_number} :"
+                f"will checkout Customer_{customer.customer_id}")
+        
+        for product in customer.products:
+
+            if cashier_number == 2:
+                product.checkout_time = 0.1
+                print("time", product.checkout_time)
+                print(f"the Cashier {cashier_number} :"
+                    f"will checkout Customer_{customer.customer_id} "
+                    f"Product_{product.product_name} "
+                    f"in {product.checkout_time}")
+                totol_time1 = round(product.checkout_time, ndigits=2)
+                final_time.append(totol_time1)
+                
+                await asyncio.sleep(product.checkout_time)
+
+            else:
+                print(f"the Cashier {cashier_number} :"
+                    f"will checkout Customer_{customer.customer_id} "
+                    f"Product_{product.product_name} "
+                    f"in {product.checkout_time + (0.1*cashier_number)} ")
+                totol_time1 = round(product.checkout_time + (0.1*cashier_number), ndigits=2)
+                final_time.append(totol_time1)
+                
+                await asyncio.sleep(product.checkout_time + (0.1*cashier_number) )
+
+        print(f"the Cashier_{cashier_number}"
+                f"finished checkout Customer_{customer.customer_id}"
+                f"in {round(time.perf_counter() - customer_start_time, ndigits=2)} secs")
+        
+        totol_time2 = round(time.perf_counter() - customer_start_time, ndigits=2)
+        # final_time.append(totol_time)
+        final_q.append(totol_time2)
+        print(f"totel time of the Cashier_{cashier_number}")
+        print(totol_time1)
+        
+        queue.task_done()
+
+    return sum(final_time), len(final_q)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# we implement the generate_customer method as a factory method for producing customers.
-#
-# We first define a product series and the required checkout time for each product. 
-# Then, we place 0 to 4 products in each customer’s shopping cart.
 def generate_customer(customer_id: int) -> Customer:
     all_products = [Product('beef', 1),
                     Product('banana', .4),
@@ -64,32 +69,56 @@ def generate_customer(customer_id: int) -> Customer:
                     Product('diapers', .2)]
     return Customer(customer_id, all_products)
 
-# we implement the customer_generation method as a producer. 
-# This method generates several customer instances regularly 
-# and puts them in the queue. If the queue is full, the put method will wait.
+
 async def customer_generation(queue: Queue, customers: int):
     customer_count = 0
     while True:
         customers = [generate_customer(the_id)
                      for the_id in range(customer_count, customer_count+customers)]
+
         for customer in customers:
-            print(f"Waiting to put Customer_{customer.customer_id} in line.... ")
+            print("Waiting to put customer in line....")
             await queue.put(customer)
-            print(f"Customer_{customer.customer_id} put in line...")
+            print("Customer pput in line....")
+
+
         customer_count = customer_count + len(customers)
         await asyncio.sleep(.001)
         return customer_count
 
-# Finally, we use the main method to initialize the queue, 
-# producer, and consumer, and start all concurrent tasks.
+
 async def main():
-    CUSTOMER = 2
-    QUEUE = 2
-    CASHIER = 2
-    customer_queue = Queue(QUEUE)
-    customers_start_time = time.perf_counter()
+    totol_time = []
+    q = []
+    customer_queue = Queue(10)
+    cashier_time = time.perf_counter()
+    customer_producer = asyncio.create_task(customer_generation(customer_queue, 10))
+    cashiers = [checkout_customer(customer_queue, i) for i in range(5)]
     
-    async with asyncio.TaskGroup() as group:
+
+    result = await asyncio.gather(customer_producer, *cashiers)
+
+    # for i in result:
+    totol_time.append(result[0])
+    q.append(result)
+    
+    #print(totol_time)
+    print("Time",q)
+    print(f"The supermarket process finished"
+           f"{customer_producer.result()} customers"
+           f"in {round(time.perf_counter() - cashier_time, ndigits=2)} secs")
+    
+    
     
 if __name__ == "__main__":
     asyncio.run(main())
+    
+# +--------|------------|-------------|-----------------------|-------------------------    
+# Queue	   | Customer   | Cashier	  |  Time each Customer	  |  Time for all Customers
+# 2	       | 2	        | 2		      |                       |           
+# 2	       | 3	        | 2		      |                       |                                               		
+# 2	       | 4	        | 2		      |                       |           
+# 2	       | 10	        | 3		      |                       |           
+# 5	       | 10	        | 4			  |                       |               
+# 5	       | 20			|             |                       |  >= 8 s
+# +--------|------------|-------------|-----------------------|-------------------------
